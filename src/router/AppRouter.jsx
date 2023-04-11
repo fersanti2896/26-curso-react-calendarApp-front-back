22236b843a6663d7b735b05b8fb7eee0609ba224
Fernando Nicolás <fersanti2896@gmail.com>
2023-04-11 00:17:29 -0600

    Modificando URLs al momento de autenticarse en AppRouter.jsx

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,8 @@
+import { useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../auth';
 import { CalendarPage } from '../calendar';
 import { useAuthStore } from '../hooks';
-import { useEffect } from 'react';
 
 export const AppRouter = () => {
     const { status, checkAuthToken } = useAuthStore();
@@ -21,11 +21,19 @@ export const AppRouter = () => {
         <Routes>
             {
                 ( status === 'not-authenticated' ) 
-                ? <Route path='/auth/*' element={ <LoginPage /> } />
-                : <Route path='/*' element={ <CalendarPage /> } />
+                ? (
+                    <>
+                        <Route path='/auth/*' element={ <LoginPage /> } />
+                        <Route path='/*' element={ <Navigate to="/auth/login" /> } />
+                    </>
+                )
+                : (
+                    <>
+                        <Route path='/' element={ <CalendarPage /> } />
+                        <Route path='/*' element={ <Navigate to="/" /> } />
+                    </>
+                )
             }
-
-            <Route path='/*' element={ <Navigate to="/auth/login" /> } />
         </Routes>
     )
 }
